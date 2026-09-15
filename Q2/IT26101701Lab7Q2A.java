1e5ee90a4b0f934e588db9903e6fcc30e9aabd4c
IT26101701 <it26101701@my.sliit.lk>
2026-09-16 04:40:48 +0530

    Add Lab7Q2A.java to Q2

---
+++ Q2/IT26101701Lab7Q2A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class IT26101701Lab7Q2A {
+	
+    public static void main(String[] args) {
+		
+		int row, column;
+		
+		//Nested for loop 
+		for (row = 1; row <= 4; row++ ){   //Outer for loop to control rows
+			
+			//Loop to print the dollar signs according to no.of rows and columns
+			for (column = 1; column <= 5; column++ ){   //Inner for loop to control column
+				
+				System.out.print("$ ");
+				
+			}
+			
+			System.out.println ();
+			
+		}
+	
+	}
+	
+}
